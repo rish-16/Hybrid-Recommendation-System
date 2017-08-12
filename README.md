3f1b25e3e0a5962d9e2c47e0a77f8faab1ccf982
Rishabh Anand <mail.rishabh.anand@gmail.com>
2017-08-13 07:21:19 +0800

    First commit: Hybrid Rec System

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hybrid-Recommendation-System
 A collaborative and content-based movie recommendation system created using Python.
+
+To run program, use python3 only (LightFM cannot be installed for python2).
+
+
---
+++ recommendations.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import lightfm
+import scipy as sp
+from lightfm.datasets import fetch_movielens
+from lightfm import LightFM
+
+# Import dataset
+dataset = fetch_movielens(min_rating=4.0)
+
+# Create model
+model = LightFM(loss='warp')
+model.fit(dataset['train'], epochs=30, num_threads=2)
+
+# Generate recommendations
+def sample_rec(model, data, user_ids):
+	n_users, n_items = dataset['train'].shape
+
+	for user_id in user_ids:
+		known_pos = dataset['item_labels'][dataset['train'].tocsr()[user_id].indices]
+
+		scores = model.predict(user_id, np.arange(n_items))
+
+		top_items = dataset['item_labels'][np.argsort(-scores)]
+
+		print ('__________________________________________________________________________')
+		print ('')
+
+		print ('User {}'.format(user_id))
+		print ('')
+		print ('Known Positives:')
+
+		for x in known_pos[:10]:
+			print ('{}'.format(x))
+
+		print ('')
+		print ('')
+
+		print ('Recommended movies:')
+
+		for x in top_items[:5]:
+			print ('{}'.format(x))
+
+		print ('')
+		print ('__________________________________________________________________________')
+
+# Provide list of various user IDs to obtain recommendations
+sample_rec(model, dataset, [3, 25, 450, 56, 183])
